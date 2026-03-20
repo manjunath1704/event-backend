76861d5d938536bc1784ea8613074a72a76614f6
Manjunath Kottarki <35862575+manjunath1704@users.noreply.github.com>
2026-03-20 16:25:40 +0530

    edge cases

--- src/controllers/eventController.js
+++ src/controllers/eventController.js
@@ -1,28 +1,36 @@
-// export const getEvents = (req, res) => {
-//     res.json({
-//       message: "All events fetched successfully 🎉",
-//       data: []
-//     });
-//   };
-  
-//   export const createEvent = (req, res) => {
-//     const event = req.body;
-  
-//     res.json({
-//       message: "Event created successfully ✅",
-//       data: event
-//     });
-//   };
 import Event from "../models/Event.js";
 
 // GET events
 export const getEvents = async (req, res) => {
-  const events = await Event.find();
-  res.json(events);
+  try {
+    const events = await Event.find();
+    res.status(200).json(events);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
 // CREATE event
 export const createEvent = async (req, res) => {
-  const event = await Event.create(req.body);
-  res.json(event);
+  try {
+    const { name, date } = req.body;
+
+    // ✅ Validation
+    if (!name || !date) {
+      return res.status(400).json({
+        message: "Name and date are required",
+      });
+    }
+
+    const event = await Event.create({ name, date });
+
+    // res.status(201).json(event);
+    res.status(201).json({
+      success: true,
+      message: "Event created successfully",
+      data: event,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
