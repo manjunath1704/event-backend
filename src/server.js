9e5d728b443b5a9f2d0a25d0d3975671c81c32a6
Manjunath Kottarki <35862575+manjunath1704@users.noreply.github.com>
2026-03-20 19:08:57 +0530

    auth with JWT

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import eventRoutes from "./routes/eventRoutes.js";
 import { connectDB } from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
 
 
 dotenv.config();
@@ -14,6 +15,7 @@ app.use(express.json());
 
 // Routes
 app.use("/api/events", eventRoutes);
+app.use("/api/auth", authRoutes);
 
 // Test route
 app.get("/", (req, res) => {
